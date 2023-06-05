03233a2a5162cd64ee4228b81d1ac3f445d74987
LucasSampaioOliveira <lucas.sampaio33@hotmail.com>
2023-06-05 18:32:17 -0300

    login social

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,30 +1,29 @@
 'use client';
 
-import { signIn } from 'next-auth/react';
-import axios from "axios";
-import { AiFillGithub } from "react-icons/ai";
-import { FcGoogle } from "react-icons/fc";
 import { useCallback, useState } from "react";
 import { toast } from "react-hot-toast";
+import { signIn } from 'next-auth/react';
 import { 
   FieldValues, 
-  SubmitHandler,
+  SubmitHandler, 
   useForm
 } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
+import { AiFillGithub } from "react-icons/ai";
+import { useRouter } from "next/navigation";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
 import Modal from "./Modal";
 import Input from "../inputs/Input";
 import Heading from "../Heading";
 import Button from "../Button";
-import useLoginModal from "@/app/hooks/useLoginModal";
-import { useRouter } from 'next/navigation';
 
 const LoginModal = () => {
   const router = useRouter();
-  const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
   const [isLoading, setIsLoading] = useState(false);
 
   const { 
@@ -39,35 +38,34 @@ const LoginModal = () => {
       password: ''
     },
   });
-
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+  
+  const onSubmit: SubmitHandler<FieldValues> = 
+  (data) => {
     setIsLoading(true);
 
-    signIn('credentials', {
-      ...data,
+    signIn('credentials', { 
+      ...data, 
       redirect: false,
     })
-
     .then((callback) => {
       setIsLoading(false);
-
 
       if (callback?.ok) {
         toast.success('Logged in');
         router.refresh();
         loginModal.onClose();
       }
-
+      
       if (callback?.error) {
         toast.error(callback.error);
       }
-    })
-
+    });
   }
 
   const onToggle = useCallback(() => {
-    registerModal.onClose();
-  }, [registerModal])
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [loginModal, registerModal])
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
@@ -79,7 +77,7 @@ const LoginModal = () => {
         id="email"
         label="Email"
         disabled={isLoading}
-        register={register}
+        register={register}  
         errors={errors}
         required
       />
@@ -102,23 +100,17 @@ const LoginModal = () => {
         outline 
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => {}} 
+        onClick={() => signIn('google')}
       />
       <Button 
         outline 
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn('github')}
       />
-      <div 
-        className="
-          text-neutral-500 
-          text-center 
-          mt-4 
-          font-light
-        "
-      >
-        <p>Already have an account?
+      <div className="
+      text-neutral-500 text-center mt-4 font-light">
+        <p>First time using Airbnb?
           <span 
             onClick={onToggle} 
             className="
@@ -126,7 +118,7 @@ const LoginModal = () => {
               cursor-pointer 
               hover:underline
             "
-            > Log in</span>
+            > Create an account</span>
         </p>
       </div>
     </div>
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -6,12 +6,12 @@ import MenuItem from './MenuItem';
 import { useCallback, useState } from 'react';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
-import { User } from "@prisma/client";
-import { signOut } from 'next-auth/react';
 
+import { signOut } from 'next-auth/react';
+import { SafeUser } from '@/app/types';
 
 interface UserMenuProps {
-    currentUser?: User | null
+    currentUser?: SafeUser | null
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({
