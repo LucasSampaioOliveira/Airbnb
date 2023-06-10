cb1c5dd49a702b0f8c3a01ecbae3a9b6e92bccfe
LucasSampaioOliveira <lucas.sampaio33@hotmail.com>
2023-06-10 11:45:39 -0300

    criando campo de titulo e descricao da casa/ap e o preco e validacao no banco de dados

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -221,6 +221,61 @@ const RentModal = () => {
     )
   }
 
+
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading 
+          title='How would you describe your place?'
+          subtitle='Short and sweet works best!'
+        />
+
+        <Input 
+          id='title'
+          label='Title'
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        
+        <hr/>
+
+        <Input 
+          id='description'
+          label='Description'
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+  }
+
+
+  if (step === STEPS.PRICE) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading 
+          title='Now, set your price'
+          subtitle='How much do you charge per night?' 
+        />
+
+        <Input 
+          id='price'
+          label='Price'
+          formatPrice
+          type='number'
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+  }
+
   return (
     <Modal
       disabled={isLoading}
